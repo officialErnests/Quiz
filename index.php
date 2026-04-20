ef9c4fff96c87900ab558f61709f1d176ce0504a
Arnis <ipa24.a.kalejs@vtdt.edu.lv>
2026-04-20 09:22:41 +0300

    Hello

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "functions.php";
 require_once "Database.php";
 $config = require "config.php";
 
-if (session_status() == 1) {
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 $db = new Database($config["database"]);
