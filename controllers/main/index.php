090c8848d34ac1542c4689864659377f31c57fab
officialErnests <ernestspursw@gmail.com>
2026-04-20 09:38:46 +0300

    made user creation

--- controllers/main/index.php
+++ controllers/main/index.php
@@ -1,3 +1,6 @@
 <?php
+
+
+
 require "./views/main/index.view.php";
     
--- controllers/main/signup.php
+++ controllers/main/signup.php
@@ -4,16 +4,26 @@ $errors = [];
 
 require "validator.php";
 
-$errors["username"] = "aaa";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!Validator::string($_POST["username"], min: 3, max: 25)) {
-
+        $errors["username"] = "username not in range of 3 - 25 charecters";
     }
-    if (!Validator::string($_POST["password"], min: 4, max: 64)) {
-
+    if ($_POST["password"] != $_POST["password_2"]) {
+        $errors["password"] = "password doesn't match";
     }
     if (!Validator::string($_POST["password_2"], min: 4, max: 64)) {
-
+        $errors["password"] = "password not in range of 4 - 25 characters";
+    }
+    if (!Validator::string($_POST["password"], min: 4, max: 64)) {
+        $errors["password"] = "password not in range of 4 - 25 characters";
+    }
+    if (empty($errors)) {
+        $sql_query = "INSERT INTO login(username, password) VALUES (:username, :password)";
+        $params = ["username" => $_POST["username"],
+                    "password" => $_POST["password"]];
+        $query = $db->query($sql_query, $params);
+        header("Location: /");
+        exit();
     }
 }
 
