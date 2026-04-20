351c4221a63bbcdd3b790139b7304175072ce809
officialErnests <ernestspursw@gmail.com>
2026-04-20 09:20:43 +0300

    finilazed prototytpe signup view

--- controllers/main/signup.php
+++ controllers/main/signup.php
@@ -1,6 +1,21 @@
 <?php 
 
+$errors = [];
 
+require "validator.php";
+
+$errors["username"] = "aaa";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!Validator::string($_POST["username"], min: 3, max: 25)) {
+
+    }
+    if (!Validator::string($_POST["password"], min: 4, max: 64)) {
+
+    }
+    if (!Validator::string($_POST["password_2"], min: 4, max: 64)) {
+
+    }
+}
 
 $pageTitle = "signup";
 $customStyles = ["main/signup.css"];
