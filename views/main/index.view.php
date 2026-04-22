499382debf966bfb3209163197392632a8e1322b
Arnis <ipa24.a.kalejs@vtdt.edu.lv>
2026-04-22 14:49:11 +0300

    Design still working

--- views/main/index.view.php
+++ views/main/index.view.php
@@ -1,12 +1,35 @@
 <?php require "views/components/header.php"; ?>
 <?php require "views/components/navbar.php"; ?>
 
-<?php
-    if (isset($_SESSION["user_id"])) {
-        echo $_SESSION["username"] . "<br>";
-        echo $_SESSION["role"] . "<br>";
-        echo $_SESSION["user_id"];
-    }
-?>
+    <div class="index_logo">
+        <h1>TRY YOUR BEST!</h1>
+        <h2> Try your abilities with QUEZZZZ
+    </div>
+
+    <div class="window_main">
+        <div class="window">
+            <h2>Play a quizes about popular themes</h2>
+        </div>
+    </div>
+
+     <div class="window_main">
+        <div class="window">
+            <h2>Try all what you can do!</h2>
+        </div>
+    </div>
+
+        <div class="window_main">
+            <div class="window">
+                <h2>Show others that you are smarther than them</h2>
+                <img src="assets/download.png" alt="smart" width="90px" height="90px">
+            </div>
+        </div>
+
+     <div class="window_main">
+        <div class="window">
+            <h2>Enjoy your free time</h2>
+        </div>
+    </div>
+
 
 <?php require "views/components/footer.php"; ?>
